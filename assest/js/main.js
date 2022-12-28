894d1385f4f22d8a1ec882137ab9a06b3c17683d
hagar hassan <hagarhassan813@gmail.com>
2022-12-28 04:18:19 +0200

    add search input

---
+++ assest/js/main.js
@@ -0,0 +1,11 @@
+// function color(){
+//     document.getElementsByClassName("custom_font").style.color = "aliceblue";
+// }
+// function changeColor(){
+//     document.getElementsByClassName("custom_font").style.color = "aliceblue";
+// }
+function concat(a){
+    return `${"something"} ${a} `
+}
+
+console.log(concat("hello"));
